b85deb1fd803f6dd78b11d818d198150c14760e8
Camilo Tapia <camilo.tapia@gmail.com>
2018-10-07 14:42:53 +0200

    add so we can use async wait if we want with normal throw errors

--- dev/index.js
+++ dev/index.js
@@ -1,3 +1,5 @@
+require('isomorphic-fetch');
+
 let ms = require('../');
 
 const start = async () => {
@@ -15,6 +17,30 @@ const start = async () => {
         res.send({hej: 1});
         next();
     });
+
+    // Normal errors
+    server.get('/error-1', (req, res, next) => {
+        next({name: 'error-1'});
+    });
+
+    // throw errors
+    server.get('/error-2', async (req, res, next) => {
+        throw new Error('error-2');
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 10));
+
+    console.log('START');
+
+    try {
+        let req1 = await fetch('http://localhost:9000/data/error-2');
+        let res1 = await req1.text();
+        console.log('res1', res1);
+    } catch (err) {
+        console.log(err);
+    }
+
+
 
 };
 
